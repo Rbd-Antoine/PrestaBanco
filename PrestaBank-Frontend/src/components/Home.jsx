c4e5cbeecc54dafb40d2164d0b0cffd825352ef6
Rbd-Antoine <122039637+Rbd-Antoine@users.noreply.github.com>
2024-11-06 16:18:19 -0300

    modif test

--- PrestaBank-Frontend/src/components/Home.jsx
+++ PrestaBank-Frontend/src/components/Home.jsx
@@ -5,7 +5,6 @@ const Home = () => {
         <h2>PrestaBanco es una entidad financiera líder en el mercado de los créditos hipotecarios.</h2>
         <p>En PrestaBanco, nos preocupamos por nuestros clientes y por eso ofrecemos una amplia gama de productos y servicios financieros.</p>
         <p>Si estás buscando una solución a tus problemas financieros, en PrestaBanco te ofrecemos la mejor opción.</p>
-        <p>¡Ven a PrestaBanco y descubre todo lo que podemos hacer por ti!</p>
       </div>
       
     );
